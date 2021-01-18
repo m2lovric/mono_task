f4dcd05e975904a98dc6fde44410e89a78e739b3
Matteo <m2lovric@gmail.com>
2021-01-18 09:08:06 +0100

    Book list updated

--- src/Components/BooksList.jsx
+++ src/Components/BooksList.jsx
@@ -1,9 +1,37 @@
-function BooksList() {
-  return (
-    <section>
+import { useObserver } from 'mobx-react-lite';
+import { useBooksContext } from '../Stores/BooksStore';
+import { useAuthorsContext } from '../Stores/AuthorsStore';
 
+const BooksList = () => {
+  const store = useBooksContext();
+  const storeAuthor = useAuthorsContext();
+
+  const getAuthor = (id) => {
+    const found = storeAuthor.authors.find(el => el.id == id);
+    return (
+      <h3>
+        {found.name}
+      </h3>
+    )
+  }
+
+  return useObserver(() => (
+    <section className="full books_grid">
+      {
+        store.books.map(el => {
+
+          return (
+            <article key={el.id} className="article">
+              <img src={el.poster} alt="poster" className="article__poster" />
+              <h2>{el.title}</h2>
+              {getAuthor(el.author)}
+              <p>{el.price}</p>
+            </article>
+          )
+        })
+      }
     </section>
-  );
+  ));
 }
 
 export default BooksList;
