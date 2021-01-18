ba18a23c055053cfeced4a7046a0dc03af9b350c
Matteo <m2lovric@gmail.com>
2021-01-18 09:10:21 +0100

    Change authors id's and addBook function

--- src/Stores/BooksStore.js
+++ src/Stores/BooksStore.js
@@ -7,31 +7,35 @@ const BooksContext = createContext();
 export const useBooksContext = () => useContext(BooksContext);
 
 export const BooksProvider = ({ children }) => {
-  const store = useLocalStore(() => {
-    const books = [
-        {
-          id: uuidv4(),
-          title: "Leviathan Wakes",
-          price: 19.0,
-          poster:
-            "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/8414/9781841499888.jpg",
-        },
-        {
-          id: uuidv4(),
-          title: "Caliban's War",
-          price: 24.5,
-          poster:
-            "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/3161/9780316129060.jpg",
-        },
-      ],
-      addBook = (title, price) => {
-        store.books.push({
-          id: uuidv4(),
-          title,
-          price,
-        });
-      };
-  });
+  const store = useLocalStore(() => ({
+    books: [
+      {
+        id: uuidv4(),
+        title: "Leviathan Wakes",
+        author: "authoridjames",
+        price: 19.0,
+        poster:
+          "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/8414/9781841499888.jpg",
+      },
+      {
+        id: uuidv4(),
+        title: "Caliban's War",
+        author: "authoridjames",
+        price: 24.5,
+        poster:
+          "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/3161/9780316129060.jpg",
+      },
+    ],
+    addBook(title, author, price, poster) {
+      store.books.push({
+        id: uuidv4(),
+        title,
+        author,
+        price,
+        poster,
+      });
+    },
+  }));
 
   return (
     <BooksContext.Provider value={store}>{children}</BooksContext.Provider>
