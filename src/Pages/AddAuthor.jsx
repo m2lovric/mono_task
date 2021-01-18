91267b3b2f0eecfe1bd70030e55b41e12194d362
Matteo <m2lovric@gmail.com>
2021-01-18 11:31:22 +0100

    Make add author function

--- src/Pages/AddAuthor.jsx
+++ src/Pages/AddAuthor.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthorsContext } from '../Stores/AuthorsStore';
 
 const AddAuthor = () => {
+  const storeAuthors = useAuthorsContext();
+
+  const [name, setName] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    storeAuthors.addAuthor(name);
+    setName('');
+  }
+
   return (
-    <form action="">
+    <form onSubmit={submitForm} className="form">
       <h1>Add Author</h1>
       <label htmlFor="name">Name: </label>
-      <input name="name" type="text" />
+      <input name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
       <button>Submit</button>
     </form>
   )
