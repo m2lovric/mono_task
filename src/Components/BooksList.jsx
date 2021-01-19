a6105c77bc979635d083b85ff58a9ba3d2b4c866
Matteo <m2lovric@gmail.com>
2021-01-19 11:53:51 +0100

    Add Title search filter

--- src/Components/BooksList.jsx
+++ src/Components/BooksList.jsx
@@ -1,10 +1,12 @@
 import { useObserver } from 'mobx-react-lite';
-import { useBooksContext } from '../Stores/BooksStore';
-import { useAuthorsContext } from '../Stores/AuthorsStore';
+import { useBooksContext } from '../Common/BooksStore';
+import { useAuthorsContext } from '../Common/AuthorsStore';
+import { useSearchContext } from '../Stores/FiltersStore';
 
 const BooksList = () => {
   const store = useBooksContext();
   const storeAuthor = useAuthorsContext();
+  const storeSearch = useSearchContext();
 
   const getAuthorsName = (id) => {
     const found = storeAuthor.authors.find(el => el.id === id);
@@ -18,17 +20,28 @@ const BooksList = () => {
   return useObserver(() => (
     <section className="full books_grid">
       {
-        store.books.map(el => {
 
-          return (
-            <article key={el.id} className="article">
-              <img src={el.poster} alt="poster" className="article__poster" />
-              <h2>{el.title}</h2>
-              {getAuthorsName(el.author)}
-              <p>{el.price}</p>
-            </article>
-          )
-        })
+        storeSearch.search ?
+          store.books.filter(el => el.title.toLowerCase().includes(storeSearch.search)).map(el => {
+            return (
+              <article key={el.id} className="article">
+                <img src={el.poster} alt="poster" className="article__poster" />
+                <h2>{el.title}</h2>
+                {getAuthorsName(el.author)}
+                <p>{el.price}</p>
+              </article>
+            )
+          })
+          : store.books.map(el => {
+            return (
+              <article key={el.id} className="article">
+                <img src={el.poster} alt="poster" className="article__poster" />
+                <h2>{el.title}</h2>
+                {getAuthorsName(el.author)}
+                <p>{el.price}</p>
+              </article>
+            )
+          })
       }
     </section>
   ));
