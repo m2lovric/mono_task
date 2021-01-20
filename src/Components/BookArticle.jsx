519ef67d51e11016bba7ba26fd6d342dd442c73e
Matteo <m2lovric@gmail.com>
2021-01-20 14:19:16 +0100

    Add Edit page Link

--- src/Components/BookArticle.jsx
+++ src/Components/BookArticle.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const BookArticle = ({ id, poster, title, getAuthorsName, author, price }) => {
   return (
-    <article key={id} className="article">
+    <article className="article">
       <img src={poster} alt="poster" className="article__poster" />
       <h2>{title}</h2>
       {getAuthorsName(author)}
       <p>{price}</p>
+      <Link to={`/edit/${id}`}>Edit</Link>
     </article>
   )
 }
